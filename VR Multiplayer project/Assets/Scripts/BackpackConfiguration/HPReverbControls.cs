68bf185b93636277b6e41f179088cf44c8002a51
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-10-14 16:00:29 +0200

    Added popup possibility

--- VR Multiplayer project/Assets/Scripts/BackpackConfiguration/HPReverbControls.cs
+++ VR Multiplayer project/Assets/Scripts/BackpackConfiguration/HPReverbControls.cs
@@ -5,9 +5,26 @@ using UnityEngine.InputSystem;
 
 public class HPReverbControls : MonoBehaviour
 {
+    GameObject textPopUp;
+
+
+    public void Start()
+    {
+
+        textPopUp = GameObject.FindGameObjectWithTag("ChoicePopUp");
+        textPopUp.SetActive(false);
+    }
     public void PressTrigger(InputAction.CallbackContext context)
     {
         Debug.Log("Trigger");
+        if (textPopUp.activeInHierarchy == false)
+        {
+            textPopUp.SetActive(true);
+        }
+        else
+        {
+            textPopUp.SetActive(false);
+        }
     }
 
     public void Joystick(InputAction.CallbackContext context)

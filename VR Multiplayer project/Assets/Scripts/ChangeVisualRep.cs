be609f7e40d477dba30197053a99779d02fde325
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-11-17 23:48:54 +0100

    added floating cylender (will be changed to arrow) when changing role

--- VR Multiplayer project/Assets/Scripts/ChangeVisualRep.cs
+++ VR Multiplayer project/Assets/Scripts/ChangeVisualRep.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class ChangeVisualRep : MonoBehaviour
 {
+    
+
+    public void Start()
+    {
+        GameObject.FindGameObjectWithTag("PointerAgressor").SetActive(false);
+        GameObject.FindGameObjectWithTag("PointerNurse").SetActive(false);
+
+    }
     // Start is called before the first frame update
     public void OnTriggerEnter(Collider other)
     {
@@ -14,10 +22,13 @@ public class ChangeVisualRep : MonoBehaviour
             if (other.gameObject.tag == "AgressorButton")
             {
                 player.GetComponent<ExecuteChangeVisualRep>().ExecuteAgressorChange(player);
+                GameObject.FindGameObjectWithTag("PointerAgressor").SetActive(true);
             }
             if (other.gameObject.tag == "NurseButton")
             {
                 player.GetComponent<ExecuteChangeVisualRep>().ExecuteNurseChange(player);
+                GameObject.FindGameObjectWithTag("PointerNurse").SetActive(true);
+
             }
         }
     }

11e090e275c554a5266174390edddefcdda4cdb8
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-11-18 14:34:18 +0100

    Debugging

--- VR Multiplayer project/Assets/Scripts/ExecuteChangeVisualRep.cs
+++ VR Multiplayer project/Assets/Scripts/ExecuteChangeVisualRep.cs
@@ -51,7 +51,7 @@ public class ExecuteChangeVisualRep : NetworkBehaviour
         visualRep.transform.GetChild(2).gameObject.SetActive(true);
         GameObject model = player.transform.GetChild(0).transform.GetChild(2).transform.GetChild(2).gameObject;
         model.transform.localPosition = new Vector3(0.035f, -0.72f, -0.12f);
-        GameManager.CheckForTwoPlayers(1); // Tell gamemanager an agressor has been initialized.
+        //GameManager.CheckForTwoPlayers(1); // Tell gamemanager an agressor has been initialized.
     }
 
     [ClientRpc]
@@ -63,6 +63,6 @@ public class ExecuteChangeVisualRep : NetworkBehaviour
         visualRep.transform.gameObject.transform.GetChild(2).gameObject.SetActive(false);
         GameObject model = player.transform.GetChild(0).transform.GetChild(2).transform.GetChild(1).gameObject;
         model.transform.localPosition = new Vector3(0.035f, -0.72f, -0.12f);
-        GameManager.CheckForTwoPlayers(2); // Tell gamemanager an agressor has been initialized.
+        //GameManager.CheckForTwoPlayers(2); // Tell gamemanager an agressor has been initialized.
     }
 }

39dd7002d34991cf8786ec0d668d918e9ae2be3e
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-12-08 16:34:59 +0100

    debug

--- VR Multiplayer project/Assets/Scripts/LoadNextScene.cs
+++ VR Multiplayer project/Assets/Scripts/LoadNextScene.cs
@@ -3,21 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class LoadNextScene : NetworkBehaviour
 {
     bool nurseCheck;
     bool agressorCheck;
-
-    GameObject gameManager;
-
-    private void Start()
-    {
-        if (this == isServer)
-        {
-            gameManager = GameObject.Find("GameManager");
-        }
-    }
+    Scene scene;
+    float timeRemaining = 70;
 
     public void OnTriggerEnter(Collider other)
     {
@@ -25,35 +16,53 @@ public class LoadNextScene : NetworkBehaviour
         {
             if (this == isServer)
             {
-                Debug.Log("NurseOnSpawn");
-                gameManager.GetComponent<GameManager>().ChangeScene(1);
+                scene = SceneManager.GetActiveScene();
+                if (scene.name == "Wachtkamer")
+                {
+                    NetworkManager.singleton.ServerChangeScene("IntroductionRoom");
+                }
+                if (scene.name == "IntroductionRoom")
+                {
+                    if (timeRemaining <= 0)
+                    {
+                        NetworkManager.singleton.ServerChangeScene("ZiekenhuisKamer");
+                    }
+                }
             }
         }
-        else if (other.tag == "Agressor")
-        {
-            if (this == isServer)
-            {
-                Debug.Log("AgressorOnSpawn");
-                gameManager.GetComponent<GameManager>().ChangeScene(2);
-            }
-        }
+        //else if (other.tag == "Agressor")
+        //{
+        //    //SceneManager.LoadScene("ZiekenhuisKamer");
+        //    if (this == isServer)
+        //    {
+        //        //NetworkManager.singleton.ServerChangeScene("ZiekenhuisKamer");
+        //        NetworkManager.singleton.ServerChangeScene("IntroductionRoom");
+        //    }
+        //}
     }
 
-    public void OnTriggerExit(Collider other)
+    private void Update()
     {
-        if (other.tag == "Nurse")
+        if (scene.name == "IntroductionRoom")
         {
-            if (this == isServer)
-            {
-                gameManager.GetComponent<GameManager>().ChangeScene(1);
-            }
-        }
-        else if (other.tag == "Agressor")
-        {
-            if (this == isServer)
-            {
-                gameManager.GetComponent<GameManager>().ChangeScene(2);
-            }
+            timeRemaining -= Time.deltaTime;
         }
     }
+    //public void OnTriggerExit(Collider other)
+    //{
+    //    if (other.tag == "Nurse")
+    //    {
+    //        if (this == isServer)
+    //        {
+    //            gameManager.GetComponent<GameManager>().PrepNextScene(1);
+    //        }
+    //    }
+    //    else if (other.tag == "Agressor")
+    //    {
+    //        if (this == isServer)
+    //        {
+    //            gameManager.GetComponent<GameManager>().PrepNextScene(2);
+    //        }
+    //    }
+    //}
 }

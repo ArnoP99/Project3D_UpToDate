9d89eec697c49c957270fd1073039322d4590895
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-12-23 15:16:38 +0100

    Debug

--- VR Multiplayer project/Assets/Scripts/ScoreCounter.cs
+++ VR Multiplayer project/Assets/Scripts/ScoreCounter.cs
@@ -1,9 +1,10 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class ScoreCounter : MonoBehaviour
+public class ScoreCounter : NetworkBehaviour
 {
     int s_agressorScore = 0;
 
@@ -18,39 +19,44 @@ public class ScoreCounter : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == 9 && this.gameObject.tag == "AgressorBox")
+        if (this == isClient && this != isServer && this == isLocalPlayer)
         {
-            s_agressorScore += 10;
-            sendScoreToPlayer(1);
-        }
-        else if (other.gameObject.layer == 9 && this.gameObject.tag == "NurseBox")
-        {
-            s_nurseScore += 10;
-            sendScoreToPlayer(0);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.layer == 9 && this.gameObject.tag == "AgressorBox")
-        {
-            if (s_agressorScore > 0)
+            if (other.gameObject.layer == 9 && this.gameObject.tag == "AgressorBox")
             {
-                s_agressorScore -= 10;
+                s_agressorScore += 10;
                 sendScoreToPlayer(1);
             }
-        }
-        else if (other.gameObject.layer == 9 && this.gameObject.tag == "NurseBox")
-        {
-            if (s_nurseScore > 0)
+            else if (other.gameObject.layer == 9 && this.gameObject.tag == "NurseBox")
             {
-                s_nurseScore -= 10;
+                s_nurseScore += 10;
                 sendScoreToPlayer(0);
             }
         }
     }
+    private void OnTriggerExit(Collider other)
+    {
+        if (this == isClient && this != isServer && this == isLocalPlayer)
+        {
+            if (other.gameObject.layer == 9 && this.gameObject.tag == "AgressorBox")
+            {
+                if (s_agressorScore > 0)
+                {
+                    s_agressorScore -= 10;
+                    sendScoreToPlayer(1);
+                }
+            }
+            else if (other.gameObject.layer == 9 && this.gameObject.tag == "NurseBox")
+            {
+                if (s_nurseScore > 0)
+                {
+                    s_nurseScore -= 10;
+                    sendScoreToPlayer(0);
+                }
+            }
+        }
+    }
 
-    private void sendScoreToPlayer(int activePlayer)
+    public void sendScoreToPlayer(int activePlayer)
     {
 
         if (activePlayer == 0)

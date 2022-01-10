24e81ce50f4aefc40f8352dfa9055b0ce66e3fe2
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2022-01-10 16:50:53 +0100

    initconvo after sanitizing hands

--- VR Multiplayer project/Assets/HandSanitizer.cs
+++ VR Multiplayer project/Assets/HandSanitizer.cs
@@ -1,15 +1,49 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandSanitizer : MonoBehaviour
+public class HandSanitizer : NetworkBehaviour
 {
+    public GameObject nurse;
+    public GameObject agressor;
+    public GameObject[] tempPlayers;
+
+    ConversationManager conversationManager;
     ParticleSystem handSanitizer;
+    bool firstTime = true;
     private void OnTriggerEnter(Collider other)
     {
         handSanitizer = GameObject.Find("Sanitizer_ParticleSystem").GetComponent<ParticleSystem>();
 
         handSanitizer.Play();
+
+        if(other.transform.parent.transform.parent.transform.GetChild(2).gameObject.tag == "Nurse" && firstTime)
+        {
+            firstTime = false;
+            InitConvo();
+        }
+
+    }
+
+    public void InitConvo()
+    {
+        conversationManager = GameObject.Find("ConversationManager").gameObject.GetComponent<ConversationManager>();
+
+        tempPlayers = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (var tempPlayer in tempPlayers)
+        {
+            if (tempPlayer.GetComponent<NetworkIdentity>().isLocalPlayer == true && tempPlayer.GetComponent<NetworkIdentity>().isClient == true && tempPlayer.transform.GetChild(0).transform.GetChild(2).gameObject.tag == "Nurse")
+            {
+                nurse = tempPlayer;
+                conversationManager.StartConversation(nurse);
+            }
+            else if (tempPlayer.GetComponent<NetworkIdentity>().isLocalPlayer == true && tempPlayer.GetComponent<NetworkIdentity>().isClient == true && tempPlayer.transform.GetChild(0).transform.GetChild(2).gameObject.tag == "Agressor")
+            {
+                agressor = tempPlayer;
+            }
+        }
     }
 }
 
--- VR Multiplayer project/Assets/Scripts/InitializeConversation.cs
+++ VR Multiplayer project/Assets/Scripts/InitializeConversation.cs
@@ -5,28 +5,9 @@ using UnityEngine;
 
 public class InitializeConversation : NetworkBehaviour
 {
-    public GameObject nurse;
-    public GameObject agressor;
-    public GameObject[] tempPlayers;
 
-    ConversationManager conversationManager;
     private void Start()
     {
-        conversationManager = GameObject.Find("ConversationManager").gameObject.GetComponent<ConversationManager>();
-
-        tempPlayers = GameObject.FindGameObjectsWithTag("Player");
-
-        foreach (var tempPlayer in tempPlayers)
-        {
-            if (tempPlayer.GetComponent<NetworkIdentity>().isLocalPlayer == true && tempPlayer.GetComponent<NetworkIdentity>().isClient == true && tempPlayer.transform.GetChild(0).transform.GetChild(2).gameObject.tag == "Nurse")
-            {
-                nurse = tempPlayer;
-                conversationManager.StartConversation(nurse);
-            }
-            else if (tempPlayer.GetComponent<NetworkIdentity>().isLocalPlayer == true && tempPlayer.GetComponent<NetworkIdentity>().isClient == true && tempPlayer.transform.GetChild(0).transform.GetChild(2).gameObject.tag == "Agressor")
-            {
-                agressor = tempPlayer;
-            }
-        }
+       
     }
 }

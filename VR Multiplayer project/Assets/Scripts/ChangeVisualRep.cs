e3a3ba7e2bace0edf9052bf2544b3247e4bff4ee
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2022-01-13 15:57:59 +0100

    debug

--- VR Multiplayer project/Assets/Scripts/ChangeVisualRep.cs
+++ VR Multiplayer project/Assets/Scripts/ChangeVisualRep.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeVisualRep : NetworkBehaviour
+public class ChangeVisualRep : MonoBehaviour
 {
     
 
@@ -22,13 +22,13 @@ public class ChangeVisualRep : NetworkBehaviour
         {
             GameObject player = this.transform.parent.transform.parent.transform.parent.gameObject;
 
-            if (other.gameObject.tag == "AgressorButton" && player.GetComponent<NetworkIdentity>().isLocalPlayer)
+            if (other.gameObject.tag == "AgressorButton")
             {
                 player.GetComponent<ExecuteChangeVisualRep>().ExecuteAgressorChange(player);
                 //GameObject.FindGameObjectWithTag("PointerAgressor").SetActive(true);
                 
             }
-            if (other.gameObject.tag == "NurseButton" && player.GetComponent<NetworkIdentity>().isLocalPlayer)
+            if (other.gameObject.tag == "NurseButton")
             {
                 player.GetComponent<ExecuteChangeVisualRep>().ExecuteNurseChange(player);
                 //GameObject.FindGameObjectWithTag("PointerNurse").SetActive(true);

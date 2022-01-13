13ef9f6b832529fcd7a841ff87bf5b745419dbb6
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2022-01-13 16:48:17 +0100

    Arrow animation & Adjusted Sliders

--- VR Multiplayer project/Assets/Scripts/ArrowAnimationAfterTimer.cs
+++ VR Multiplayer project/Assets/Scripts/ArrowAnimationAfterTimer.cs
@@ -1,32 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ArrowAnimationAfterTimer : MonoBehaviour
 {
-
+    Scene scene;
     float timeRemaining = 5;
 
     MeshRenderer pointerNurse;
     MeshRenderer pointerAgressor;
+
+    bool isLoaded = true;
     // Start is called before the first frame update
     void Start()
     {
-        pointerAgressor = GameObject.Find("ArrowAgressor").gameObject.GetComponent<MeshRenderer>();
-        pointerNurse = GameObject.Find("ArrowNurse").gameObject.GetComponent<MeshRenderer>();
-        pointerAgressor.enabled = false;
-        pointerNurse.enabled = false;
+
 
         // Update is called once per frame
         void Update()
         {
-
-            timeRemaining -= Time.deltaTime;
-            if (timeRemaining <= 0)
+            if (scene.name == "IntroductionRoom")
             {
-                pointerAgressor.enabled = true;
-                pointerNurse.enabled = true;
+                if (isLoaded)
+                {
+                    pointerAgressor = GameObject.Find("ArrowAgressor").gameObject.GetComponent<MeshRenderer>();
+                    pointerNurse = GameObject.Find("ArrowNurse").gameObject.GetComponent<MeshRenderer>();
+                    pointerAgressor.enabled = false;
+                    pointerNurse.enabled = false;
+                    isLoaded = false;
+                }
+                timeRemaining -= Time.deltaTime;
+                if (timeRemaining <= 0)
+                {
+                    pointerAgressor.enabled = true;
+                    pointerNurse.enabled = true;
+                }
             }
+
 
         }
     }

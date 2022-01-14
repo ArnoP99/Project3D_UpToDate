eb5d85cbc77f3810f063e7a876826d8d2d2d6ebe
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2022-01-14 13:33:28 +0100

    Updated scorecounter

--- VR Multiplayer project/Assets/Scripts/ScoreCounter.cs
+++ VR Multiplayer project/Assets/Scripts/ScoreCounter.cs
@@ -7,8 +7,6 @@ using UnityEngine;
 public class ScoreCounter : NetworkBehaviour
 {
     int s_agressorScore = 0;
-
-
     int s_nurseScore = 0;
 
     GameObject player;
@@ -22,43 +20,89 @@ public class ScoreCounter : NetworkBehaviour
         Debug.Log(other.gameObject);
         if (other.gameObject.layer == 9 && this.gameObject.tag == "AgressorBox")
         {
-            s_agressorScore += 10;
-            sendScoreToPlayer(1);
+            if (other.gameObject.tag == "Low")
+            {
+                s_agressorScore += 10;
+                sendScoreToPlayer(1);
+            }
+            if (other.gameObject.tag == "Medium")
+            {
+                s_agressorScore += 20;
+                sendScoreToPlayer(1);
+            }
+            if (other.gameObject.tag == "High")
+            {
+                s_agressorScore += 30;
+                sendScoreToPlayer(1);
+            }
         }
         else if (other.gameObject.layer == 9 && this.gameObject.tag == "NurseBox")
         {
-            s_nurseScore += 10;
-            sendScoreToPlayer(0);
+            if (other.gameObject.tag == "Low")
+            {
+                s_nurseScore += 10;
+                sendScoreToPlayer(0);
+            }
+            if (other.gameObject.tag == "Medium")
+            {
+                s_nurseScore += 20;
+                sendScoreToPlayer(0);
+            }
+            if (other.gameObject.tag == "High")
+            {
+                s_nurseScore += 30;
+                sendScoreToPlayer(0);
+            }
         }
-
     }
     private void OnTriggerExit(Collider other)
     {
-
         if (other.gameObject.layer == 9 && this.gameObject.tag == "AgressorBox")
         {
             if (s_agressorScore > 0)
             {
-                s_agressorScore -= 10;
-                sendScoreToPlayer(1);
+                if (other.gameObject.tag == "Low")
+                {
+                    s_agressorScore -= 10;
+                    sendScoreToPlayer(1);
+                }
+                if (other.gameObject.tag == "Medium")
+                {
+                    s_agressorScore -= 20;
+                    sendScoreToPlayer(1);
+                }
+                if (other.gameObject.tag == "High")
+                {
+                    s_agressorScore -= 30;
+                    sendScoreToPlayer(1);
+                }
             }
         }
         else if (other.gameObject.layer == 9 && this.gameObject.tag == "NurseBox")
         {
             if (s_nurseScore > 0)
             {
-                s_nurseScore -= 10;
-                sendScoreToPlayer(0);
+                if (other.gameObject.tag == "Low")
+                {
+                    s_nurseScore -= 10;
+                    sendScoreToPlayer(0);
+                }
+                if (other.gameObject.tag == "Medium")
+                {
+                    s_nurseScore -= 20;
+                    sendScoreToPlayer(0);
+                }
+                if (other.gameObject.tag == "High")
+                {
+                    s_nurseScore -= 30;
+                    sendScoreToPlayer(0);
+                }
             }
-
         }
     }
 
     public void sendScoreToPlayer(int activePlayer)
     {
-        Debug.Log("ActivePlayer: " + activePlayer);
-
-
         if (activePlayer == 0)
         {
             GameObject nurseBar = GameObject.Find("NurseBar");
@@ -66,7 +110,6 @@ public class ScoreCounter : NetworkBehaviour
             nurseBar.GetComponent<ScoreBar>().SetScore(s_nurseScore);
             Debug.Log("score nurse: " + s_nurseScore);
             player = GameObject.FindGameObjectWithTag("Nurse").transform.parent.transform.parent.gameObject;
-            //Debug.Log("player: " + player.tag);
             if (player.GetComponent<NetworkIdentity>().isLocalPlayer)
             {
                 player.GetComponent<AssignAuth>().ExecuteCmdSendPlayerScore(s_nurseScore, 0);
@@ -77,9 +120,7 @@ public class ScoreCounter : NetworkBehaviour
             GameObject agressorBar = GameObject.Find("AgressorBar");
             Debug.Log("score agressor: " + s_agressorScore);
             agressorBar.GetComponent<ScoreBar>().SetScore(s_agressorScore);
-
-            player = GameObject.FindGameObjectWithTag("Agressor").transform.parent.transform.parent.gameObject; 
-            //Debug.Log("player: " + player.tag);
+            player = GameObject.FindGameObjectWithTag("Agressor").transform.parent.transform.parent.gameObject;
             if (player.GetComponent<NetworkIdentity>().isLocalPlayer)
             {
                 player.GetComponent<AssignAuth>().ExecuteCmdSendPlayerScore(s_agressorScore, 1);

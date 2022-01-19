f372fc481255abd7c72141594672d792dca6a86e
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2022-01-19 15:12:21 +0100

    debug

--- VR Multiplayer project/Assets/Scripts/Timer.cs
+++ VR Multiplayer project/Assets/Scripts/Timer.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class Timer : NetworkBehaviour
 {
-    float timeRemaining = 300;
+    float timeRemaining = 60;
     bool timerIsRunning = false;
 
     GameObject minutes0;
